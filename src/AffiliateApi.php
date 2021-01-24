31a46ab3ee972997434438c67980e8b97e845029
Bilal Eren <bilaleren@users.noreply.github.com>
2021-01-24 22:46:32 +0300

    Removed multiple uses

--- src/AffiliateApi.php
+++ src/AffiliateApi.php
@@ -3,58 +3,35 @@
 namespace Tune;
 
 use Tune\Utils\UseApiCalls;
-use Tune\Controllers\Affiliate\AdManager;
-use Tune\Controllers\Affiliate\AffiliateBilling;
-use Tune\Controllers\Affiliate\AffiliateUser;
-use Tune\Controllers\Affiliate\Alert;
-use Tune\Controllers\Affiliate\ApiKey;
-use Tune\Controllers\Affiliate\Application;
-use Tune\Controllers\Affiliate\BrandDesign;
-use Tune\Controllers\Affiliate\BrandInformation;
-use Tune\Controllers\Affiliate\Dne;
-use Tune\Controllers\Affiliate\Goal;
-use Tune\Controllers\Affiliate\Notification;
-use Tune\Controllers\Affiliate\NotificationCenter;
-use Tune\Controllers\Affiliate\Offer;
-use Tune\Controllers\Affiliate\OfferFile;
-use Tune\Controllers\Affiliate\OfferPixel;
-use Tune\Controllers\Affiliate\OfferTargeting;
-use Tune\Controllers\Affiliate\OfferUrl;
-use Tune\Controllers\Affiliate\PrivacyPolicy;
-use Tune\Controllers\Affiliate\Report;
-use Tune\Controllers\Affiliate\RingRevenue;
-use Tune\Controllers\Affiliate\SavedReports;
-use Tune\Controllers\Affiliate\ScheduledOfferChange;
-use Tune\Controllers\Affiliate\Tag;
-use Tune\Controllers\Affiliate\UserPreferences;
+use Tune\Controllers\Affiliate;
 
 /**
  * @see https://developers.tune.com/affiliate/
- * @method AdManager adManager()
- * @method Controllers\Affiliate\Affiliate affiliate()
- * @method AffiliateBilling affiliateBilling()
- * @method AffiliateUser affiliateUser()
- * @method Alert alert()
- * @method ApiKey apiKey()
- * @method Application application()
- * @method BrandDesign brandDesign()
- * @method BrandInformation brandInformation()
- * @method Dne dne()
- * @method Goal goal()
- * @method Notification notification()
- * @method NotificationCenter notificationCenter()
- * @method Offer offer()
- * @method OfferFile offerFile()
- * @method OfferPixel offerPixel()
- * @method OfferTargeting offerTargeting()
- * @method OfferUrl offerUrl()
- * @method PrivacyPolicy privacyPolicy()
- * @method Report report()
- * @method RingRevenue ringRevenue()
- * @method SavedReports savedReports()
- * @method ScheduledOfferChange scheduledOfferChange()
- * @method Tag tag()
- * @method UserPreferences userPreferences()
+ * @method Affiliate\AdManager adManager()
+ * @method Affiliate\Affiliate affiliate()
+ * @method Affiliate\AffiliateBilling affiliateBilling()
+ * @method Affiliate\AffiliateUser affiliateUser()
+ * @method Affiliate\Alert alert()
+ * @method Affiliate\ApiKey apiKey()
+ * @method Affiliate\Application application()
+ * @method Affiliate\BrandDesign brandDesign()
+ * @method Affiliate\BrandInformation brandInformation()
+ * @method Affiliate\Dne dne()
+ * @method Affiliate\Goal goal()
+ * @method Affiliate\Notification notification()
+ * @method Affiliate\NotificationCenter notificationCenter()
+ * @method Affiliate\Offer offer()
+ * @method Affiliate\OfferFile offerFile()
+ * @method Affiliate\OfferPixel offerPixel()
+ * @method Affiliate\OfferTargeting offerTargeting()
+ * @method Affiliate\OfferUrl offerUrl()
+ * @method Affiliate\PrivacyPolicy privacyPolicy()
+ * @method Affiliate\Report report()
+ * @method Affiliate\RingRevenue ringRevenue()
+ * @method Affiliate\SavedReports savedReports()
+ * @method Affiliate\ScheduledOfferChange scheduledOfferChange()
+ * @method Affiliate\Tag tag()
+ * @method Affiliate\UserPreferences userPreferences()
  */
 class AffiliateApi extends UseApiCalls
 {
--- src/NetworkApi.php
+++ src/NetworkApi.php
@@ -3,97 +3,54 @@
 namespace Tune;
 
 use Tune\Utils\UseApiCalls;
-use Tune\Controllers\Network\AdManager;
-use Tune\Controllers\Network\Advertiser;
-use Tune\Controllers\Network\AdvertiserBilling;
-use Tune\Controllers\Network\AdvertiserUser;
-use Tune\Controllers\Network\Affiliate;
-use Tune\Controllers\Network\AffiliateBilling;
-use Tune\Controllers\Network\AffiliateUser;
-use Tune\Controllers\Network\Alert;
-use Tune\Controllers\Network\ApiKey;
-use Tune\Controllers\Network\Application;
-use Tune\Controllers\Network\Authentication;
-use Tune\Controllers\Network\CashflowGroup;
-use Tune\Controllers\Network\Conversion;
-use Tune\Controllers\Network\ConversionMeta;
-use Tune\Controllers\Network\CustomMenuLink;
-use Tune\Controllers\Network\CustomPage;
-use Tune\Controllers\Network\CustomerList;
-use Tune\Controllers\Network\DneList;
-use Tune\Controllers\Network\DownloadReport;
-use Tune\Controllers\Network\Employee;
-use Tune\Controllers\Network\Goal;
-use Tune\Controllers\Network\InsertionOrder;
-use Tune\Controllers\Network\InsertionOrderOffer;
-use Tune\Controllers\Network\Notification;
-use Tune\Controllers\Network\NotificationCenter;
-use Tune\Controllers\Network\Offer;
-use Tune\Controllers\Network\OfferDisabledLink;
-use Tune\Controllers\Network\OfferFile;
-use Tune\Controllers\Network\OfferPixel;
-use Tune\Controllers\Network\OfferTargeting;
-use Tune\Controllers\Network\OfferUrl;
-use Tune\Controllers\Network\OfferWhitelist;
-use Tune\Controllers\Network\Optimizer;
-use Tune\Controllers\Network\OptimizerExclusion;
-use Tune\Controllers\Network\OptimizerNetworkPreference;
-use Tune\Controllers\Network\Preference;
-use Tune\Controllers\Network\RawLog;
-use Tune\Controllers\Network\Report;
-use Tune\Controllers\Network\RingRevenue;
-use Tune\Controllers\Network\SavedReports;
-use Tune\Controllers\Network\ScheduledOfferChange;
-use Tune\Controllers\Network\Tag;
-use Tune\Controllers\Network\TinyUrl;
-use Tune\Controllers\Network\VatRate;
+use Tune\Controllers\Network;
 
 /**
  * @see https://developers.tune.com/network/
- * @method AdManager adManager()
- * @method Advertiser advertiser()
- * @method AdvertiserBilling advertiserBilling()
- * @method AdvertiserUser advertiserUser()
- * @method Affiliate affiliate()
- * @method AffiliateBilling affiliateBilling()
- * @method AffiliateUser affiliateUser()
- * @method Alert alert()
- * @method ApiKey apiKey()
- * @method Application application()
- * @method Authentication authentication()
- * @method CashflowGroup cashflowGroup()
- * @method Conversion conversion()
- * @method ConversionMeta conversionMeta()
- * @method CustomMenuLink customMenuLink()
- * @method CustomPage customPage()
- * @method CustomerList customerList()
- * @method DneList dneList()
- * @method DownloadReport downloadReport()
- * @method Employee employee()
- * @method Goal goal()
- * @method InsertionOrder insertionOrder()
- * @method InsertionOrderOffer insertionOrderOffer()
- * @method Notification notification()
- * @method NotificationCenter notificationCenter()
- * @method Offer offer()
- * @method OfferDisabledLink offerDisabledLink()
- * @method OfferFile offerFile()
- * @method OfferPixel offerPixel()
- * @method OfferTargeting offerTargeting()
- * @method OfferUrl offerUrl()
- * @method OfferWhitelist offerWhitelist()
- * @method Optimizer optimizer()
- * @method OptimizerExclusion optimizerExclusion()
- * @method OptimizerNetworkPreference optimizerNetworkPreference()
- * @method Preference preference()
- * @method RawLog rawLog()
- * @method Report report()
- * @method RingRevenue ringRevenue()
- * @method SavedReports savedReports()
- * @method ScheduledOfferChange scheduledOfferChange()
- * @method Tag tag()
- * @method TinyUrl tinyUrl()
- * @method VatRate vatRate()
+ * @method Network\AdManager adManager()
+ * @method Network\Advertiser advertiser()
+ * @method Network\AdvertiserBilling advertiserBilling()
+ * @method Network\AdvertiserUser advertiserUser()
+ * @method Network\Affiliate affiliate()
+ * @method Network\AffiliateBilling affiliateBilling()
+ * @method Network\AffiliateUser affiliateUser()
+ * @method Network\Alert alert()
+ * @method Network\ApiKey apiKey()
+ * @method Network\Application application()
+ * @method Network\Authentication authentication()
+ * @method Network\CashflowGroup cashflowGroup()
+ * @method Network\Conversion conversion()
+ * @method Network\ConversionMeta conversionMeta()
+ * @method Network\CustomMenuLink customMenuLink()
+ * @method Network\CustomPage customPage()
+ * @method Network\CustomerList customerList()
+ * @method Network\DneList dneList()
+ * @method Network\DownloadReport downloadReport()
+ * @method Network\Employee employee()
+ * @method Network\Goal goal()
+ * @method Network\InsertionOrder insertionOrder()
+ * @method Network\InsertionOrderOffer insertionOrderOffer()
+ * @method Network\Notification notification()
+ * @method Network\NotificationCenter notificationCenter()
+ * @method Network\Offer offer()
+ * @method Network\OfferDisabledLink offerDisabledLink()
+ * @method Network\OfferFile offerFile()
+ * @method Network\OfferPixel offerPixel()
+ * @method Network\OfferTargeting offerTargeting()
+ * @method Network\OfferUrl offerUrl()
+ * @method Network\OfferWhitelist offerWhitelist()
+ * @method Network\Optimizer optimizer()
+ * @method Network\OptimizerExclusion optimizerExclusion()
+ * @method Network\OptimizerNetworkPreference optimizerNetworkPreference()
+ * @method Network\Preference preference()
+ * @method Network\RawLog rawLog()
+ * @method Network\Report report()
+ * @method Network\RingRevenue ringRevenue()
+ * @method Network\SavedReports savedReports()
+ * @method Network\ScheduledOfferChange scheduledOfferChange()
+ * @method Network\Tag tag()
+ * @method Network\TinyUrl tinyUrl()
+ * @method Network\VatRate vatRate()
  */
 class NetworkApi extends UseApiCalls
 {
